11018ac543200b54895a7933ea26922176b2ff1b
erolerme <erolerme@gmail.com>
2019-01-27 16:47:58 +0300

    Operatörler

---
+++ operators.playground/Contents.swift
@@ -0,0 +1,49 @@
+/*
+ 
+ Swift dilinde operatörler
+ 
+ Mantıksal Operatörler
+ 
+ */
+
+//Toplama
+var sonuc = 3 + 5
+
+//Çıkarma
+sonuc = 6 - 5
+
+//Çarpma
+sonuc = 3 * 7
+
+//Bolme
+var bolMeSonucu:Float = Float(24) / Float(12)
+/*
+ Bölme sonucu virgüllü olabileceğinden dolayı tip dönüşümü ile bölünenler float tipine dönüştürülür ve sonuç yine float tipine eşitlenir.
+ */
+
+//Mod Alma
+var modSonucu = 27 % 13
+
+//Kendisiyle Toplama
+var sayi = 12
+sayi += 4
+print(sayi)
+
+//Kendisinden Çıkarma
+sayi -= 6
+print(sayi)
+
+//Operatörler ve Değişkenlerin Kullanımına Örnekler
+let sayi1 = 12
+let sayi2 = 4
+let carpimSonucu = sayi1 * sayi2
+print(carpimSonucu)
+
+
+
+
+
+
+
+
+
